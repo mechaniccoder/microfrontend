56297d7570efb22a115222022dc9bf1b44f7780b
mechaniccoder <yuseunghwan94@gmail.com>
2021-11-28 10:52:17 +0900

    deploy: marketing

--- app/marketing/webpack.prod.js
+++ app/marketing/webpack.prod.js
@@ -10,7 +10,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "marketing/latest/",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
